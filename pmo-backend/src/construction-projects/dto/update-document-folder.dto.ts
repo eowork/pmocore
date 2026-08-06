aa7265aa017895744a8e4c78582fa99f87c618cd
takumis <takumis@example.com>
2026-08-06 08:08:42 +0800

    feat: push all  changes show after I build the application.

--- pmo-backend/src/construction-projects/dto/update-document-folder.dto.ts
+++ pmo-backend/src/construction-projects/dto/update-document-folder.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDocumentFolderDto } from './create-document-folder.dto';
 
-export class UpdateDocumentFolderDto extends PartialType(CreateDocumentFolderDto) {}
+export class UpdateDocumentFolderDto extends PartialType(
+  CreateDocumentFolderDto,
+) {}
